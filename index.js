70059b40d3ee53f9a9724cba58b6eb8296201729
Jony Choi <jony.choi7@gmail.com>
2020-04-29 10:12:35 -0500

    Secret info protection

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 var express = require("express");
 var app = express();
 const mongoose = require("mongoose");
+const { User } = require("./models/User");
+const bodyParser = require("body-parser");
+const config = require("./config/key");
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
 mongoose
-  .connect(
-    "mongodb+srv://kory7:jiworld92@boilerplate-lpzrm.mongodb.net/test?retryWrites=true&w=majority",
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true,
-      useFindAndModify: false,
-    }
-  )
+  .connect(config.mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
   .then(() => {
     console.log("MongoDB connected...");
   })
@@ -20,6 +23,16 @@ mongoose
 
 app.get("/", function (req, res) {
   res.send("Hello World!");
+});
+
+app.post("/register", (req, res) => {
+  const user = new User(req.body);
+  user.save((err, userInfo) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).json({
+      success: true,
+    });
+  });
 });
 
 app.listen(3000, function () {
